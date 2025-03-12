b76db12ce3c3926268afe998a5f6b735e32b4d29
dan2-ux <danhlanhat9@gmail.com>
2025-03-12 14:30:59 +0700

    EditT.jsx

--- src/pages/EditT.jsx
+++ src/pages/EditT.jsx
@@ -14,7 +14,7 @@ function EditT({id}){
         }
         sLoading(true)
         axios
-            .put(`http://localhost:555/app/books/${id}`, data)
+            .put(`http://localhost:5555/app/books/${id}`, data)
             .then(() => {
                 sLoading(false);
                 navigate(0);
